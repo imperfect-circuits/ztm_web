390be59d737ccee6b16d0c42c5219481524d1473
unknown <114847048+imperfect-circuits@users.noreply.github.com>
2023-12-22 11:45:27 +1030

    Adding class toggle and close buttons
    Added buttons to html
    Added handlers for adding the 'done' class and the button closure.
    Adding the handlers both to the existing list items, and to new ones.
    Added the closure button to new items.
    Added (not required) preventing of removing item from the list if it has not been marked as done.

--- domevents/script.js
+++ domevents/script.js
@@ -7,9 +7,23 @@ function inputLength() {
 }
 
 function createListElement() {
+	// create list element
 	var li = document.createElement("li");
 	li.appendChild(document.createTextNode(input.value));
+
+	// create button to add to list element for closure
+	var closeButton = document.createElement("button");
+	closeButton.appendChild(document.createTextNode("X"));
+	closeButton.addEventListener("click",deleteListItem);
+	li.appendChild(closeButton);
+
+	// add toggle to show if item is done
+	li.addEventListener("click", markAsDone);
+
+	// add to list
 	ul.appendChild(li);
+
+	// clear current input
 	input.value = "";
 }
 
@@ -25,6 +39,30 @@ function addListAfterKeypress(event) {
 	}
 }
 
+function markAsDone(event) {
+	event.target.classList.toggle("done");
+}
+
+function deleteListItem(event) {
+	// don't delete if the line is not marked as done
+	var currentListItem = event.target.parentElement;
+	if (!currentListItem.classList.contains("done")) {
+		alert("Can't remove item if not done.");
+		return;
+	}
+	ul.removeChild(currentListItem);
+}
+
 button.addEventListener("click", addListAfterClick);
 
 input.addEventListener("keypress", addListAfterKeypress);
+
+// add event listeners to the list items and close buttons
+for (child of ul.children) {
+	// add 'done' toggle to item
+	child.addEventListener("click", markAsDone);
+};
+for (closeButton of document.querySelectorAll("ul li button")) {
+	// add 'done' toggle to item
+	closeButton.addEventListener("click", deleteListItem);
+};
